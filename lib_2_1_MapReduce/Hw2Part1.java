4725ffc72e89b234360feea1a9c80fb2180ba30d
yulong <liangyulong928@qq.com>
2023-04-10 09:25:12 +0800

    初次试运行得到理想输出,但仍有待解决问题如下： 1、key-value输出结果中间存在制表符,不符合输出格式 2、对于脏数据(通信双方为同一个人,通话时长不符合规则)的问题没有进行处理 3、是否可以在Map过程中将通话时长作为value进一步优化

--- lib_2_1_MapReduce/Hw2Part1.java
+++ lib_2_1_MapReduce/Hw2Part1.java
@@ -16,71 +16,65 @@ import org.apache.hadoop.mapred.TextOutputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Hw2Part1 {
-    public static class TokenizerMapper extends Mapper<Object, Text, Text, FloatWritable> {
+    public static class TokenizerMapper extends Mapper<Object, Text, Text, Text> {
         private Text word = new Text();
+        private Text one = new Text();
 
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        public void map(Object key, Text value, Context context) throws IOException, InterruptedException, NumberFormatException {
             String valueList = value.toString();
-            String[] lines = valueList.split("\n");
-            for(String line : lines){
-                String[] split = line.split(" ");
-                if (split.length != 3){
-                    continue;
-                }
-                word.set(split[0]+" "+split[1]);
-                context.write(word,new FloatWritable(Float.valueOf(split[2])));
+            StringTokenizer itr = new StringTokenizer(value.toString(),"\n");
+            while (itr.hasMoreTokens()) {
+                String[] token = itr.nextToken().split(" ");
+                word.set(token[0]+" "+token[1]);
+                one.set(token[2]);
+                context.write(word,one);
             }
         }
     }
 
-    public static class TimeAveCombiner extends Reducer<Text, FloatWritable, Text, FloatWritable> {
-        private FloatWritable result = new FloatWritable();
+    public static class CountCombiner extends Reducer<Text, Text, Text, Text> {
         private Text countToken = new Text();
+        private Text result = new Text();
 
-        public void reduce(Text key, Iterable<FloatWritable> values, Context context) throws IOException, InterruptedException {
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
             int count = 0;
             float alltime = 0;
-            for (FloatWritable val : values) {
+            for (Text val : values) {
                 count += 1;
-                alltime += val.get();
+                alltime += Float.parseFloat(val.toString());
             }
-            result.set(alltime / count);
-            String keyString = key.toString();
-            keyString = keyString + " "+count;
-            countToken.set(keyString);
+            result.set(String.valueOf(count)+" "+String.valueOf(alltime));
+            countToken.set(key.toString());
             context.write(countToken, result);
         }
     }
 
-    public static class TimeAveReducer extends Reducer<Text, FloatWritable, Text, Text> {
+    public static class TimeAveReducer extends Reducer<Text, Text, Text, Text> {
         private Text result_key = new Text();
         private Text result_value = new Text();
-        private byte[] prefix;
         private byte[] suffix;
 
         protected void setup(Context context) {
-            try {
-                prefix = new byte[0];
-                suffix = Text.encode(" ").array();
-            } catch (Exception e) {
-                prefix = suffix = new byte[0];
-            }
+            Configuration conf = context.getConfiguration();
+            conf.set("mapreduce.output.textoutputformat.separator", " ");
         }
 
-        public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
-            int sum = 0;
-            for (IntWritable val : values) {
-                sum += val.get();
+        public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            int count = 0;
+            float alltime = 0;
+            for (Text val : values) {
+                String result = val.toString();
+                String[] split = result.split(" ");
+                count += Integer.parseInt(split[0]);
+                alltime += Float.parseFloat(split[1]);
             }
-            result_key.set(prefix);
-            result_key.append(key.getBytes(), 0, key.getLength());
-            result_key.append(suffix, 0, suffix.length);
-            result_value.set(Integer.toString(sum));
+            result_key.set(key);
+            result_value.set(String.valueOf(count) + " " + String.valueOf(alltime/count));
             context.write(result_key, result_value);
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, NumberFormatException{
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         if (otherArgs.length < 2) {
@@ -90,10 +84,10 @@ public class Hw2Part1 {
         Job job = Job.getInstance(conf, "hw2part1");
         job.setJarByClass(Hw2Part1.class);
         job.setMapperClass(Hw2Part1.TokenizerMapper.class);
-        job.setCombinerClass(Hw2Part1.TimeAveCombiner.class);
+        job.setCombinerClass(Hw2Part1.CountCombiner.class);
         job.setReducerClass(Hw2Part1.TimeAveReducer.class);
         job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(IntWritable.class);
+        job.setMapOutputValueClass(Text.class);
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         for (int i = 0; i < otherArgs.length - 1; ++i) {
