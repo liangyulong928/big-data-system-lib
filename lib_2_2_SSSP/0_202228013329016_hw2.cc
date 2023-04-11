d04a8c7211b09644c5842c4636610a3077d64dbe
yulong <liangyulong928@qq.com>
2023-04-11 18:14:12 +0800

    完成标注和文档书写

--- lib_2_2_SSSP/0_202228013329016_hw2.cc
+++ lib_2_2_SSSP/0_202228013329016_hw2.cc
@@ -13,42 +13,49 @@
 
 int64_t m_v0_id = 0;
 
-class VERTEX_CLASS_NAME(InputFormatter): public InputFormatter {
+class VERTEX_CLASS_NAME(InputFormatter) : public InputFormatter
+{
 public:
-    int64_t getVertexNum() {
+    int64_t getVertexNum()
+    {
         unsigned long long n;
         sscanf(m_ptotal_vertex_line, "%lld", &n);
-        m_total_vertex= n;
+        m_total_vertex = n;
         return m_total_vertex;
     }
-    int64_t getEdgeNum() {
+    int64_t getEdgeNum()
+    {
         unsigned long long n;
         sscanf(m_ptotal_edge_line, "%lld", &n);
-        m_total_edge= n;
+        m_total_edge = n;
         return m_total_edge;
     }
-    int getVertexValueSize() {
+    int getVertexValueSize()
+    {
         m_n_value_size = sizeof(double);
         return m_n_value_size;
     }
-    int getEdgeValueSize() {
+    int getEdgeValueSize()
+    {
         m_e_value_size = sizeof(double);
         return m_e_value_size;
     }
-    int getMessageValueSize() {
+    int getMessageValueSize()
+    {
         m_m_value_size = sizeof(double);
         return m_m_value_size;
     }
-    void loadGraph() {
+    void loadGraph()
+    {
         unsigned long long last_vertex;
         unsigned long long from;
         unsigned long long to;
         double weight = 0;
-        
+
         double value = 1;
         int outdegree = 0;
-        
-        const char *line= getEdgeLine();
+
+        const char *line = getEdgeLine();
 
         // Note: modify this if an edge weight is to be read
         //       modify the 'weight' variable
@@ -58,18 +65,22 @@ public:
 
         last_vertex = from;
         ++outdegree;
-        for (int64_t i = 1; i < m_total_edge; ++i) {
-            line= getEdgeLine();
+        for (int64_t i = 1; i < m_total_edge; ++i)
+        {
+            line = getEdgeLine();
 
             // Note: modify this if an edge weight is to be read
             //       modify the 'weight' variable
 
             sscanf(line, "%lld %lld %lf", &from, &to, &weight);
-            if (last_vertex != from) {
+            if (last_vertex != from)
+            {
                 addVertex(last_vertex, &value, outdegree);
                 last_vertex = from;
                 outdegree = 1;
-            } else {
+            }
+            else
+            {
                 ++outdegree;
             }
             addEdge(from, to, &weight);
@@ -78,14 +89,17 @@ public:
     }
 };
 
-class VERTEX_CLASS_NAME(OutputFormatter): public OutputFormatter {
+class VERTEX_CLASS_NAME(OutputFormatter) : public OutputFormatter
+{
 public:
-    void writeResult() {
+    void writeResult()
+    {
         int64_t vid;
         double value;
         char s[1024];
 
-        for (ResultIterator r_iter; ! r_iter.done(); r_iter.next() ) {
+        for (ResultIterator r_iter; !r_iter.done(); r_iter.next())
+        {
             r_iter.getIdValue(vid, &value);
             int n = sprintf(s, "%lld: %f\n", (unsigned long long)vid, value);
             writeNextResLine(s, n);
@@ -94,78 +108,107 @@ public:
 };
 
 // An aggregator that records a double value tom compute sum
-class VERTEX_CLASS_NAME(Aggregator): public Aggregator<double> {
+class VERTEX_CLASS_NAME(Aggregator) : public Aggregator<double>
+{
 public:
-    void init() {
+    void init()
+    {
         m_global = 0;
         m_local = 0;
     }
-    void* getGlobal() {
+    void *getGlobal()
+    {
         return &m_global;
     }
-    void setGlobal(const void* p) {
-        m_global = * (double *)p;
+    void setGlobal(const void *p)
+    {
+        m_global = *(double *)p;
     }
-    void* getLocal() {
+    void *getLocal()
+    {
         return &m_local;
     }
-    void merge(const void* p) {
-        m_global += * (double *)p;
+    void merge(const void *p)
+    {
+        m_global += *(double *)p;
     }
-    void accumulate(const void* p) {
-        m_local += * (double *)p;
+    void accumulate(const void *p)
+    {
+        m_local += *(double *)p;
     }
 };
 
-class VERTEX_CLASS_NAME(): public Vertex <double, double, double> {
+class VERTEX_CLASS_NAME() : public Vertex<double, double, double>
+{
 public:
-    void compute(MessageIterator* pmsgs) {
+    void compute(MessageIterator *pmsgs)
+    {
         double val;
-        if (getSuperstep() == 0) {
-           if (getVertexId()==m_v0_id){
+        // Set the initial value of each vertex at the super step to 0, the source point to 0, and the remaining points to MAX
+        if (getSuperstep() == 0)
+        {
+            if (getVertexId() == m_v0_id)
+            {
                 val = 0;
-           }
-           else{
+            }
+            else
+            {
                 val = MAX;
-           }
+            }
         }
-        else {
-            if (getSuperstep() >= 2) {
-                double global_val = * (double *)getAggrGlobal(0);
-                if (global_val < EPS) {
-                    voteToHalt(); return;
+        else
+        {
+            // After the superstep is 2, the convergence is judged for each point, and the convergent point is changed to inactive
+            if (getSuperstep() >= 2)
+            {
+                double global_val = *(double *)getAggrGlobal(0);
+                if (global_val < EPS)
+                {
+                    voteToHalt();
+                    return;
                 }
             }
             val = getValue();
-            for ( ; ! pmsgs->done(); pmsgs->next() ) {
-                if(val > pmsgs->getValue()){
+            // Each edge of the point is judged, and the node value is updated if a smaller length is passed in
+            for (; !pmsgs->done(); pmsgs->next())
+            {
+                if (val > pmsgs->getValue())
+                {
                     val = pmsgs->getValue();
                 }
             }
+            // Cumulative loss degree
             double acc = fabs(getValue() - val);
             accumulateAggr(0, &acc);
         }
-        * mutableValue() = val;
+        // Write the update node value to the node in the graph
+        *mutableValue() = val;
         OutEdgeIterator out_edge_iter = getOutEdgeIterator();
-        for(; !out_edge_iter.done(); out_edge_iter.next()){
+        // Send a message to the outgoing edge
+        for (; !out_edge_iter.done(); out_edge_iter.next())
+        {
+            // The message is "The shortest distance between you and the source point".
             double msg_value = out_edge_iter.getValue() + getValue();
-        	if(getValue() != MAX){
-        		sendMessageTo(out_edge_iter.target(),msg_value);
-        	}
+            if (getValue() != MAX)
+            {
+                sendMessageTo(out_edge_iter.target(), msg_value);
+            }
         }
     }
 };
 
-class VERTEX_CLASS_NAME(Graph): public Graph {
+class VERTEX_CLASS_NAME(Graph) : public Graph
+{
 public:
-    VERTEX_CLASS_NAME(Aggregator)* aggregator;
+    VERTEX_CLASS_NAME(Aggregator) * aggregator;
 
 public:
     // argv[0]: SSSP.so
     // argv[1]: <input path>
     // argv[2]: <output path>
     // argv[3]: <v0 id>
-    void init(int argc, char* argv[]) {
+    void init(int argc, char *argv[])
+    {
 
         setNumHosts(5);
         setHost(0, "localhost", 1411);
@@ -174,9 +217,10 @@ public:
         setHost(3, "localhost", 1441);
         setHost(4, "localhost", 1451);
 
-        if (argc < 4) {
-           printf ("Usage: %s <input path> <output path> <v0 id>\n", argv[0]);
-           exit(1);
+        if (argc < 4)
+        {
+            printf("Usage: %s <input path> <output path> <v0 id>\n", argv[0]);
+            exit(1);
         }
 
         m_pin_path = argv[1];
@@ -188,14 +232,16 @@ public:
         regAggr(0, &aggregator[0]);
     }
 
-    void term() {
+    void term()
+    {
         delete[] aggregator;
     }
 };
 
 /* STOP: do not change the code below. */
-extern "C" Graph* create_graph() {
-    Graph* pgraph = new VERTEX_CLASS_NAME(Graph);
+extern "C" Graph *create_graph()
+{
+    Graph *pgraph = new VERTEX_CLASS_NAME(Graph);
 
     pgraph->m_pin_formatter = new VERTEX_CLASS_NAME(InputFormatter);
     pgraph->m_pout_formatter = new VERTEX_CLASS_NAME(OutputFormatter);
@@ -204,9 +250,10 @@ extern "C" Graph* create_graph() {
     return pgraph;
 }
 
-extern "C" void destroy_graph(Graph* pobject) {
-    delete ( VERTEX_CLASS_NAME()* )(pobject->m_pver_base);
-    delete ( VERTEX_CLASS_NAME(OutputFormatter)* )(pobject->m_pout_formatter);
-    delete ( VERTEX_CLASS_NAME(InputFormatter)* )(pobject->m_pin_formatter);
-    delete ( VERTEX_CLASS_NAME(Graph)* )pobject;
+extern "C" void destroy_graph(Graph *pobject)
+{
+    delete (VERTEX_CLASS_NAME() *)(pobject->m_pver_base);
+    delete (VERTEX_CLASS_NAME(OutputFormatter) *)(pobject->m_pout_formatter);
+    delete (VERTEX_CLASS_NAME(InputFormatter) *)(pobject->m_pin_formatter);
+    delete (VERTEX_CLASS_NAME(Graph) *)pobject;
 }
--- lib_2_2_SSSP/README.md
+++ lib_2_2_SSSP/README.md
@@ -54,3 +54,98 @@ public:
 };
 ```
 
+## 三、编写SSSP代码
+
+1、对示例代码文件输入部分进行修改，确保边的权重存储在图中
+
+```c++
+class VERTEX_CLASS_NAME(InputFormatter): public InputFormatter {
+public:
+  ...
+  void loadGraph() {
+    ...
+    const char *line= getEdgeLine();
+    //将文件中的边值存储在weight中
+    sscanf(line, "%lld %lld %lf", &from, &to, &weight);
+    addEdge(from, to, &weight);
+    ...
+    for (int64_t i = 1; i < m_total_edge; ++i) {
+      line= getEdgeLine();
+      //扫描行时将边值存储在weight中
+      sscanf(line, "%lld %lld %lf", &from, &to, &weight);
+      ...
+    }
+};
+```
+
+2、修改位置变量个数并提取出位置变量3(起始点)
+
+```c++
+class VERTEX_CLASS_NAME(Graph): public Graph {
+public:
+  void init(int argc, char* argv[]) {
+    ...
+    //位置参数除输入文件、输出路径外，还有源点
+    if (argc < 4) {
+      printf ("Usage: %s <input path> <output path> <v0 id>\n", argv[0]);
+      exit(1);
+    }
+    m_pin_path = argv[1];
+    m_pout_path = argv[2];
+    //源点要以整型进行存储
+    m_v0_id = atoi(argv[3]);
+  }
+  ...
+};
+```
+
+3、编写SSSP核心代码
+
+```c++
+class VERTEX_CLASS_NAME(): public Vertex <double, double, double> {
+public:
+  void compute(MessageIterator* pmsgs) {
+    double val;
+    if (getSuperstep() == 0) {
+      //在超步为0设置各顶点初始值,源点为0,其余点为MAX
+      if (getVertexId()==m_v0_id){
+        val = 0;
+      }
+      else{
+        val = MAX;
+      }
+    }
+    else {
+      //在超步为2之后,对每个点判断收敛性,对收敛的点将其变为不活跃
+      if (getSuperstep() >= 2) {
+        double global_val = * (double *)getAggrGlobal(0);
+        if (global_val < EPS) {
+          voteToHalt(); return;
+        }
+      }
+      val = getValue();
+      //对点各入度边进行判定,有更小的长度传入则对节点值进行更新
+      for ( ; ! pmsgs->done(); pmsgs->next() ) {
+        if(val > pmsgs->getValue()){
+          val = pmsgs->getValue();
+        }
+      }
+      //累计损失度
+      double acc = fabs(getValue() - val);
+      accumulateAggr(0, &acc);
+    }
+    //将更新节点值写入图中节点
+    * mutableValue() = val;
+    OutEdgeIterator out_edge_iter = getOutEdgeIterator();
+    //向出度边发送消息
+    for(; !out_edge_iter.done(); out_edge_iter.next()){
+      //消息为"你经过我到源点的最短距离"
+      double msg_value = out_edge_iter.getValue() + getValue();
+      //对非初始化的节点不更新
+      if(getValue() != MAX){
+        sendMessageTo(out_edge_iter.target(),msg_value);
+      }
+    }
+  }
+};
+```
